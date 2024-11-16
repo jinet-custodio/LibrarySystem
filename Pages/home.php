acfddb0f2a7f1657ffef7c388028eeae1adfeed8
Bartolome, Louise Anne S. <louisebartolome.basc@gmail.com>
2024-11-17 01:47:49 +0800

    git commit

--- Pages/home.php
+++ Pages/home.php
@@ -11,82 +11,35 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library System</title>
-    <link rel="icon" type="image/x-icon" href="../Assets/Images/bookshelf.png" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="/assets/css/index.css" />
-    <style>
-        h1,
-        h3 {
-            text-align: center !important;
-            width: 100%;
-        }
-
-        .container {
-            width: 40% !important;
-            background-color: rgba(206, 206, 206, 0.842);
-            border-radius: 2vw;
-            float: left;
-            display: grid;
-            margin-left: 2vw !important;
-            margin-bottom: 2vw !important;
-        }
-
-        .btn {
-            width: 40% !important;
-            margin: 10px auto;
-        }
-
-        .count {
-            float: right !important;
-            margin-right: 2vw !important;
-            display: block;
-        }
-
-        .cards {
-            display: flex;
-            flex-wrap: wrap;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .card {
-            padding: 10px;
-            border: 2px solid black !important;
-            text-align: center;
-            height: 120px !important;
-            width: 40% !important;
-            border-radius: 1vw;
-            margin: 1vw;
-            font-size: 1.5vw !important;
-        }
-
-        .numbers {
-            background-color: white;
-            width: 50%;
-            padding: 10px 0;
-            margin: 0 auto;
-        }
-    </style>
+    <link rel="icon" type="image/x-icon" href="../Asset/Images/bookshelf.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../Asset/CSS/home.css" />
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-primary">
-        <span class="navbar-text">
-            Dashboard
-        </span>
-        <a type="button" id="logout">Log Out</a>
-        <!-- <a class="navbar-brand fixed-top py-lg-0 padding" href="#"> -->
-        <!-- <img src=" ../Assets/Images/logo.jpg" alt="basc-logo" class="d-inline-block align-top img-circle"> -->
-        </a>
-    </nav>
+    <form action="../function/logOut.php" method="POST">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: rgb(248, 238, 238);">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="../Asset/Images/library.png" alt="Logo" width="80" height="auto" class="d-inline-block">
+                    BASC LIBRARY
+                </a>
 
-    <h1>BASC Library System</h1>
+                <nav class="navbar bg-body-tertiary">
+                    <form class="container-fluid justify-content-start">
+                        <button class="btn btn-outline-success me-2" type="submit" id="logOut" value="logOut"
+                            name="logOut">Log Out</button>
 
-    <div class="container">
-        <h3>Books</h3>
-        <?php
+                    </form>
+                </nav>
+            </div>
+        </nav>
+
+
+        <div class="sidebar">
+            <?php
         //add userID to redirect link
         $query = "SELECT * FROM category ORDER BY `category`.`id` ASC";
         $result = mysqli_query($conn, $query);
@@ -94,31 +47,62 @@ session_start();
             foreach ($result as $category) {
                 //add userid get variable to the href using & (example href="../index.php?category=<?= $category['abbrev']?greaterthan & userID =<?= $userID ? =greaterthan)
         ?>
-                <a href="booklist.php?category=<?= $category['abbrev'] ?>" type="button" class="btn btn-primary"><?= $category['name'] ?> </a>
-        <?php
+            <a href="booklist.php?category=<?= $category['abbrev'] ?>" type="button"><?= $category['name'] ?> </a>
+            <?php
             }
         } else {
             echo "<h5> No Record Found </h5>";
         }
         ?>
-    </div>
-    <div class="count container">
-        <h3>Accounts</h3>
-        <div class="cards">
-            <div class="card">
-                <p> Students </p>
-                <div class="numbers"><?= 22 ?> </div>
-            </div>
-            <div class="card">
-                <p> Faculty </p>
-                <div class="numbers"><?= 12 ?> </div>
-            </div>
-            <div class="card">
-                <p> Staff </p>
-                <div class="numbers"><?= 4 ?> </div>
-            </div>
         </div>
-    </div>
+
+        <h1>Welcome, Bookworm!</h1>
+
+        <div class="count-container">
+            <h3>Registered Accounts</h3>
+            <div class="card" style="width: 15rem;">
+                <img class="card-img-top" src="../Asset/Images/student.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Students</h5>
+                    <div class="numbers"><?= 22 ?> </div>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <img class="card-img-top" src="../Asset/Images/faculty.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Faculty</h5>
+                    <div class="numbers"><?= 12?> </div>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <img class="card-img-top" src="../Asset/Images/employee.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Staff</h5>
+                    <div class="numbers"><?= 4?> </div>
+                </div>
+            </div>
+
+
+            <!-- <div class="cards">
+                <div class="card">
+                    <p> Students </p>
+                    <div class="numbers"><?= 22 ?> </div>
+                </div>
+                <div class="card">
+                    <p> Faculty </p>
+                    <div class="numbers"><?= 12 ?> </div>
+                </div>
+                <div class="card">
+                    <p> Staff </p>
+                    <div class="numbers"><?= 4 ?> </div>
+                </div>
+            </div> -->
+        </div>
+
+
+
+
+        <script src="../assets/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
